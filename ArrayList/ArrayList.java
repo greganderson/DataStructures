a16d7c5f08753612128a489528033308bf1493e3
Greg Anderson <greg@infobytes.com>
2015-10-16 15:29:15 -0600

    Added add method.

--- ArrayList/ArrayList.java
+++ ArrayList/ArrayList.java
@@ -1,18 +1,33 @@
 public class ArrayList<E> {
+
+	private E[] array;
+	private int lastIndex;
 	
 	public ArrayList() {
-		//
+		array = new E[256];
+		lastIndex = 0;
 	}
 
-	public boolean add(E item) {
-		// TODO: Implement
+	public void add(E item) {
+		try {
+			array[lastIndex] = item;
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			expandArray();
+			array[lastIndex] = item;
+		}
+		lastIndex++;
 	}
 
-	public boolean add(E item) {
+	public boolean remove(E item) {
 		// TODO: Implement
 	}
 
 	public boolean empty() {
+		// TODO: Implement
+	}
+
+	private void expandArray() {
 		// TODO: Implement
 	}
 }
