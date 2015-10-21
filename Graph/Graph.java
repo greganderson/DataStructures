03f992e645d95bfcb3e70d6cb7e4d7b80466b4fd
Greg Anderson <greg@infobytes.com>
2015-10-21 02:07:53 -0600

    Close #3 implemented graph.

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,62 +1,76 @@
 public class Graph<E> {
 
+	private Map<E, Set<E>> graph;	// Map<Vertex, Edges>, symbol table
+	private int numEdges;
+
 	public Graph() {
-		// TODO: Implement
+		graph = new HashMap<>();
+		numEdges = 0;
 	}
 
 	/**
 	 * Add edge v-w
 	 */
 	public void addEdge(E v, E w) {
-		// TODO: Implement
+		if (!hasVertex(v))
+			graph.put(v, new HashSet<>());
+		if (!hasEdge(v, w))
+			numEdges++;
+		graph.get(v).add(w);
 	}
 
 	/**
 	 * Number of vertices
 	 */
 	public int V() {
-		// TODO: Implement
+		return graph.size();
 	}
 
 	/**
 	 * Number of edges
 	 */
 	public int E() {
-		// TODO: Implement
+		return numEdges;
 	}
 
 	/**
 	 * Vertices in the graph
 	 */
 	public Iterable<E> vertices() {
-		// TODO: Implement
+		return graph.keySet();
 	}
 
 	/**
 	 * Neighbors of v
 	 */
 	public Iterable<E> adjacentTo(E v) {
-		// TODO: Implement
+		if (!hasVertex(v))
+			return new HashSet<>();
+		return graph.get(v);
 	}
 
 	/**
 	 * Number of neighbors of v
 	 */
 	public int degree(E v) {
-		// TODO: Implement
+		if (!hasVertex(v))
+			return 0;
+		return graph.get(v).size();
 	}
 
 	/**
 	 * Is v a vertex in the graph?
 	 */
 	public boolean hasVertex(E v) {
-		// TODO: Implement
+		return graph.containsKey(v);
 	}
 
 	/**
 	 * is v-w an edge in the graph?
 	 */
 	public boolean hasEdge(E v, E w) {
-		// TODO: Implement
+		if (!hasVertex(v))
+			return false;
+		return graph.get(v).contains(w);
 	}
 }
