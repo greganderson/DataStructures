610a96a466393e78304e55bfcff4ca33fc08d93a
Greg Anderson <greg@infobytes.com>
2015-10-28 11:05:28 -0600

    Stubbed out a hashtable for #4

---
+++ HashTable/HashTable.java
@@ -0,0 +1,26 @@
+public class HashTable<E> {
+	
+	public HashTable() {
+		// TODO: Implement
+	}
+
+	public void put(E item) {
+		// TODO: Implement
+	}
+
+	public E get(E item) {
+		// TODO: Implement
+	}
+
+	public int size() {
+		// TODO: Implement
+	}
+
+	public Iterable<E> getKeys() {
+		// TODO: Implement
+	}
+
+	public boolean isEmpty() {
+		// TODO: Implement
+	}
+}
